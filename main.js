f190c58da63eeffa595ee0bbf83348880f359b17
MiftikCZ <89579269+MiftikCZ@users.noreply.github.com>
2023-02-06 11:29:53 +0100

    Update main.js
    added description for apps

--- main.js
+++ main.js
@@ -3,217 +3,248 @@ var _miftikcz_project_list = [
       "MiftikWeb",
       [
         "miftik.tk",
-        ";mweb;pro;2021;otherlinks;web;"
+        ";mweb;pro;2021;otherlinks;web;",
+        "My official website",
       ]
     ],
     [
       "Miftik Army",
       [
-        "https://discord.com/invite/SDxqN8qab2",
-        ";discord;pro;og;comunity;"
+        "discord.com/invite/SDxqN8qab2",
+        ";discord;pro;og;comunity;",
+        "My official discord server",
       ]
     ],
     [
       "Idea Keeper",
       [
         "miftikcz.github.io/idea-keeper",
-        ";github.io;pro;2022;tool;personal;web;"
+        ";github.io;pro;2022;tool;personal;web;",
+        "Todo app but optimized for saving ideas, not todos",
       ]
     ],
     [
       "Drawing",
       [
         "miftikcz.github.io/whiteboard",
-        ";github.io;2022;tool;personal;web;"
+        ";github.io;2022;tool;personal;web;",
+        "Svg based whiteboard web application",
       ]
     ],
     [
       "Duck Clicker",
       [
         "miftikcz.github.io/kachna-clicker",
-        ";github.io;2022;game;web;"
+        ";github.io;2022;game;web;",
+        "Random clicker game about clicking a duck",
       ]
     ],
     [
       "Games",
       [
         "miftik.tk/fun",
-        ";mweb;game;otherlinks;web;"
+        ";mweb;game;otherlinks;web;",
+        "Games created by me and links for other games made by others",
       ]
     ],
     [
       "Oats",
       [
         "miftikcz.github.io/vlocky",
-        ";github.io;semipro;web;2022;personal;food;"
+        ";github.io;semipro;web;2022;personal;food;",
+        "Some recipes for great breakfast oatmeal",
       ]
     ],
     [
       "Fast Learn",
       [
         "miftik.tk/fastLearn",
-        ";mweb;2022;semipro;web;school;personal;edu;"
+        ";mweb;2022;semipro;web;school;personal;edu;",
+        "App about writing down questions and testing if you know answer or not",
       ]
     ],
     [
       "Counter",
       [
         "miftik.tk/counter",
-        ";mweb;2022;semipro;tool;personal;web;"
+        ";mweb;2022;tool;personal;web;",
+        "Simple counting app that saves the data",
       ]
     ],
     [
       "Bot Tutorial",
       [
         "github.com/MiftikCZ/bot-tutorial",
-        ";github.com;discord;2021;tutorial;personal;web;"
+        ";github.com;discord;2021;tutorial;personal;web;",
+        "Simple tutorial how to make bot using MiftikCMD",
       ]
     ],
     [
       "MiftikRaid",
       [
         "github.com/MiftikCZ/miftikraid",
-        ";github.com;pro;discord;2021;tutorial;personal;tool;cli;"
+        ";github.com;pro;discord;2021;tutorial;personal;tool;cli;",
+        "My own free discord raiding tool",
       ]
     ],
     [
       "Webhook Spam",
       [
         "github.com/MiftikCZ/WebhookSpammer",
-        ";github.com;discord;2021;tutorial;personal;web;tool;"
+        ";github.com;discord;2021;tutorial;personal;web;tool;",
+        "My own free discord webhook spam tool",
       ]
     ],
     [
       "Gym Help",
       [
         "miftik.tk/posilovani",
-        ";mweb;gym;2022;tutorial;personal;web;"
+        ";mweb;gym;2022;tutorial;personal;web;",
+        "Some tips for beginners in the gym",
       ]
     ],
     [
       "Gym Server",
       [
         "miftik.tk/gym",
-        ";mweb;gym;2022;tutorial;personal;discord;comunity;"
+        ";mweb;gym;2022;tutorial;personal;discord;comunity;",
+        "Small fitness cominuty about sharing tips and your photos",
       ]
     ],
     [
       "Gym Counter",
       [
         "miftikcz.github.io/sets-counter/",
-        ";gym;github.io;pro;2022;tool;personal;web;"
+        ";gym;github.io;pro;2022;tool;personal;web;",
+        "Counting app especially optimized for counting sets in the gym",
       ]
     ],
     [
         "StatsPlus",
         [
             "stats.miftikcz.repl.co/",
-            ";replit;discord;2021;personal;tool;"
+            ";replit;semipro;discord;2021;personal;tool;",
+            "Discord bot for monitoring server statistics",
         ]
     ],
     [
       "JS Terminal",
       [
         "miftikcz.github.io/chslonk/",
-        ";github.io;2023;tool;cli;personal;web;"
+        ";github.io;2023;tool;cli;personal;web;",
+        "Simple for fun terminal project made with chsLang",
       ]
     ],
     [
       "MC Pack",
       [
         "www.mediafire.com/file/v5o4gmqxz7ej4ym/%25C2%25A71MiftikCZ_Pack%25C2%25A78.zip/file",
-        ";custom;2021;personal;web;"
+        ";custom;2021;personal;web;",
+        "My own minecraft resource pack",
       ]
     ],
     [
       "Drinko",
       [
         "miftikcz.github.io/drinko-app/",
-        ";gym;github.io;pro;2022;tool;personal;web;"
+        ";gym;github.io;pro;2022;tool;personal;web;",
+        "App where you can count how much water have you drank",
       ]
     ],
     [
       "URL Short",
       [
         "c.miftik.tk",
-        ";mweb;2022;tool;personal;web;"
+        ";mweb;2022;tool;personal;web;",
+        "Simple api for shorting long url's",
       ]
     ],
     [
       "Shortcuts",
       [
         "miftikcz.github.io/MiftikCZ/html-shortcuts",
-        ";github.io;2022;otherlinks;personal;web;"
+        ";github.io;2022;otherlinks;personal;web;",
+        "Light app you can use as an shortcuts",
       ]
     ],
     [
       "Startpage",
       [
         "miftikcz.github.io/homepage",
-        ";github.io;2022;custom;personal;web;"
+        ";github.io;2022;custom;personal;web;",
+        "Custom starting page made for mobile devices",
       ]
     ],
     [
       "Startpage 2",
       [
         "miftikcz.github.io/startpage-2",
-        ";github.io;2023;custom;personal;web;"
+        ";github.io;2023;custom;personal;web;",
+        "Custom starting page especially made for firefox with great ui",
       ]
     ],
     [
       "Pinguin",
       [
         "github.com/MiftikCZ/pinguin",
-        ";github.com;2023;tool;personal;cli;"
+        ";github.com;2023;tool;personal;cli;",
+        "Lightweight Arch Linux pacman client for easier user experience with AUR helper",
       ]
     ],
     [
       "EdLinkMe",
       [
         "edlinkme.miftik.tk/",
-        ";mweb;2021;tool;personal;web;"
+        ";mweb;2021;tool;personal;web;",
+        "Encrypt-Decrypt Base64 for encoding simple words or making thumbnails",
       ]
     ],
     [
       "Image Manager",
       [
         "https://replit.com/@MiftikCZ/Image-Manager",
-        ";replit;2021;tool;personal;web;"
+        ";replit;2021;tool;personal;web;",
+        "Very simple tool for transporting .png pictures across devices",
       ]
     ],
     [
       "Token Checker",
       [
         "https://replit.com/@MiftikCZ/TokensChecker#index.html",
-        ";replit;2021;tool;personal;web;"
+        ";replit;2021;tool;personal;web;",
+        "Tool for checking if your discord tokens works",
       ]
     ],
     [
       "Web Unblocker",
       [
         "miftik.tk/ub/",
-        ";mweb;2022;school;tool;personal;web;"
+        ";mweb;2022;school;tool;personal;web;",
+        "Frontend app that creates link with google translate proxy",
       ]
     ],
     [
       "Notes",
       [
         "miftik.tk/notepad/",
-        ";mweb;2022;tool;personal;web;"
+        ";mweb;2022;tool;personal;web;",
+        "Simple note taking app that saves data",
       ]
     ],
     [
       "Dirty Script",
       [
         "github.com/MiftikCZ/dirty-script",
-        ";github.com;semipro;2022;tool;dev;"
+        ";github.com;semipro;2022;tool;dev;",
+        "Powerful frontend html alternative made for simplicity and dynamic web apps.",
       ]
     ],
     [
       "Pattern",
       [
         "github.com/MiftikCZ/kresleniPattern",
-        ";github.com;2022;math;personal;web;"
+        ";github.com;2022;math;personal;web;",
+        "For fun project about drawing patterns with your mouse",
       ]
     ]
   ]
